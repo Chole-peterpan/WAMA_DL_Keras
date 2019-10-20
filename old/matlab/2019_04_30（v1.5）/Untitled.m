70105ff6c6699df626f1da43366de5c780114320
WAMAWAMA <wmy19970215@gmail.com>
2019-10-20 16:04:32 +0800

    清理文件

--- old/matlab/2019_04_30（v1.5）/Untitled.m
+++
@@ -1,8 +0,0 @@
-clear all
-
-a = 4920591;
-b = -2000481;
-
-pa = exp(a)/(exp(a)+exp(b));
-pb = exp(b)/(exp(a)+exp(b));
-
--- old/matlab/a_loss_2019_0424(v1.0)/binary_cross_entropy.m
+++
@@ -1,5 +0,0 @@
-function [loss] = binary_cross_entropy(pre,label)
-%
-loss = abs(-1*label*log(pre)+(1-label)*log(1-pre));
-end
-
